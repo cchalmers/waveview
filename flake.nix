3f0acb30332dcfa8f7bfe2d14b971498f5adaffc
Christopher Chalmers <c.chalmers@me.com>
2022-09-24 18:11:37 +0100

    Failed attempt at getting wasm build
    It's still trying to build for arch64. Tried to follow https://www.tweag.io/blog/2022-09-22-rust-nix/.

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    # cargo2nix.url = "github:torhovland/cargo2nix/wasm";
     cargo2nix.inputs.nixpkgs.follows = "nixpkgs";
     cargo2nix.inputs.flake-utils.follows = "flake-utils";
     cargo2nix.inputs.rust-overlay.follows = "rust-overlay";
@@ -57,6 +58,45 @@
             ];
           };
         in (rustPkgs.workspace.waveview {}).bin;
+
+        waveview-wasm = let
+          rustVersion = "1.63.0";
+          pkgsWasm = import nixpkgs {
+            inherit system;
+            crossSystem = {
+              config = "wasm32-unknown-wasi-unknown";
+              system = "wasm32-wasi";
+              useLLVM = true;
+            };
+            overlays = [cargo2nix.overlays.default];
+          };
+
+          rustWithWasmTarget = pkgs.rust-bin.stable.${rustVersion}.default.override {
+            targets = [ wasmTarget ];
+          };
+
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            inherit rustVersion;
+            packageFun = import ./Cargo.nix;
+          };
+
+          rustPkgsWasm = pkgsWasm.rustBuilder.makePackageSet {
+            inherit rustVersion;
+            target = "wasm32-unknown-unknown";
+
+            # cargo2nix thinks we're building for wasm32-unknown-wasi now.
+            # We need to guide it to wasm32-unknown-unknown instead.
+            # packageFun = import ./Cargo.nix;
+            packageFun = attrs: import ./Cargo.nix (attrs // {
+              hostPlatform = attrs.hostPlatform // {
+                parsed = attrs.hostPlatform.parsed // {
+                  kernel.name = "unknown";
+                };
+              };
+            });
+          };
+
+        in (rustPkgsWasm.workspace.waveview {}).bin;
 
       };
   });
