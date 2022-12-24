e36438d0a4dfe1efab651d6a75098b035bacb8e2
Christopher Chalmers <c.chalmers@me.com>
2022-12-24 02:26:52 +0000

    cargo upgrade --incompatible

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ struct Opt {
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let opt = Opt::from_args();
+    let opt = Opt::parse();
     color_eyre::install().unwrap();
     // let mut file = std::fs::File::open("/Users/chris/Dev/egui/eframe_template/clkdiv2n_tb.vcd").unwrap();
     // signals.iter().for_each(|sig| eprintln!("{:?}", sig.0.scopes));
@@ -54,25 +54,28 @@ fn main() {
     let signals = vec![];
 
     let web_options = eframe::WebOptions::default();
-    eframe::start_web(
-        "the_canvas_id", // hardcode it
-        web_options,
-        Box::new(|cc| {
-            let app = waveview::TemplateApp::new(signals, 1);
-            if let Some(Ok(s)) = web_sys::window().map(|w| w.location().search()) {
-                if let Some(url) = s.strip_prefix('?') {
-                    let request = ehttp::Request::get(url);
-                    let dl = app.download.clone();
-                    *dl.lock().unwrap() = waveview::app::Download::InProgress;
-                    let ctx = cc.egui_ctx.clone();
-                    ehttp::fetch(request, move |response| {
-                        *dl.lock().unwrap() = waveview::app::Download::Done(response);
-                        ctx.request_repaint();
-                    });
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::start_web(
+            "the_canvas_id", // hardcode it
+            web_options,
+            Box::new(|cc| {
+                let app = waveview::TemplateApp::new(signals, 1);
+                if let Some(Ok(s)) = web_sys::window().map(|w| w.location().search()) {
+                    if let Some(url) = s.strip_prefix('?') {
+                        let request = ehttp::Request::get(url);
+                        let dl = app.download.clone();
+                        *dl.lock().unwrap() = waveview::app::Download::InProgress;
+                        let ctx = cc.egui_ctx.clone();
+                        ehttp::fetch(request, move |response| {
+                            *dl.lock().unwrap() = waveview::app::Download::Done(response);
+                            ctx.request_repaint();
+                        });
+                    }
                 }
-            }
-            Box::new(app)
-        }),
-    )
-    .expect("failed to start eframe");
+                Box::new(app)
+            }),
+        )
+        .await
+        .expect("failed to start eframe");
+    });
 }
