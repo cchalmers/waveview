7ce8a21fbfc841666019b2f09f2d4dfde671bab3
Christopher Chalmers <c.chalmers@me.com>
2022-12-28 11:59:37 +0000

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,10 @@ struct Opt {
     starting_file: Option<std::path::PathBuf>,
 }
 
-// When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     let opt = Opt::parse();
     color_eyre::install().unwrap();
-    // let mut file = std::fs::File::open("/Users/chris/Dev/egui/eframe_template/clkdiv2n_tb.vcd").unwrap();
-    // signals.iter().for_each(|sig| eprintln!("{:?}", sig.0.scopes));
-    // eprintln!("{signals:?}");
-    // let app = eframe_template::TemplateApp::new(signals, time);
     tracing_subscriber::fmt()
         .with_writer(std::io::stderr)
         .init();
@@ -27,8 +22,6 @@ fn main() {
         "waveview",
         native_options,
         Box::new(move |_cc| {
-            // cc.egui_ctx.set_visuals(eframe::egui::Visuals::light());
-            // let path = opt.starting_file.().unwrap_or();
             let mut file = if let Some(path) = &opt.starting_file {
                 std::fs::File::open(path).unwrap()
             } else {
@@ -40,13 +33,9 @@ fn main() {
     );
 }
 
-// when compiling to web using trunk.
 #[cfg(target_arch = "wasm32")]
 fn main() {
-    // Make sure panics are logged using `console.error`.
     console_error_panic_hook::set_once();
-
-    // Redirect tracing to console.log and friends:
     tracing_wasm::set_as_global_default();
 
     tracing::event!(tracing::Level::INFO, "tracing says hi");
